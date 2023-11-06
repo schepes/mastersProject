aab7ea9371240ebffafc2d9ca95fb39a834c88f4
Diego Bobrow <diegobobrow25@gmail.com>
2023-11-05 23:01:48 -0500

    feat: xcode setup

---
+++ masterProjectSwift/masterProjectSwift/masterProjectSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  masterProjectSwiftApp.swift
+//  masterProjectSwift
+//
+//  Created by Diego Bobrow on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct masterProjectSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
