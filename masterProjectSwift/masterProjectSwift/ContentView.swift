c315fb702f0009348ce60f0f81d780e15599fec3
Diego Bobrow <diegobobrow25@gmail.com>
2023-11-10 01:28:18 -0500

    feat:added backend, decided to create as a mono repo

--- masterProjectSwift/masterProjectSwift/ContentView.swift
+++ masterProjectSwift/masterProjectSwift/ContentView.swift
@@ -1,17 +1,19 @@
 //
-//  ContentView.swift
+//
 //  masterProjectSwift
 //
 //  Created by Diego Bobrow on 11/5/23.
 //
 
-import SwiftUI
+
+
 
 struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
+                
                 .foregroundStyle(.tint)
             Text("Hello, world!")
         }
